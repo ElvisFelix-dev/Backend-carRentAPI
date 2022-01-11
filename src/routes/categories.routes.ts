b5f111af8703e5929f95dabf90fbad8d62f1e4ec
Elvis Felix <elvisjaspion@gmal.com>
2022-01-10 22:40:05 -0300

    Refactoring code

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,13 @@
 import { Router } from 'express'
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository'
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
+import { createCategoryController } from '../modules/cars/UseCases/createCategory'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body
-
-  const categoryAlreadyExists = categoriesRepository.findByName(name)
-
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category already exists!"})
-  }
-
-  categoriesRepository.create({ name, description})
-
-  return response.status(201).send()
+  return createCategoryController.handle(request, response)
 })
 
 categoriesRoutes.get('/', (request, response) => {
